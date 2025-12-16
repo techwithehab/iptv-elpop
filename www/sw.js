dc73bde52aa23c2584dc65db9c1c89d3d406e6ea
techwithehab <tech.with.ehab@gmail.com>
2025-12-16 22:49:32 +0200

    Add files via upload

--- www/sw.js
+++ www/sw.js
@@ -3,7 +3,7 @@ const ASSETS_TO_CACHE = [
     './', // الصفحة الرئيسية
     './index.html', // اسم ملفك (تأكد من الاسم)
     'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css',
-    'https://cdn.jsdelivr.net/npm/hls.js@latest'
+     './assets/libs/hls.min.js',
     // لا تضع ملفات Tailwind CDN هنا لأنها تتغير، الأفضل تحميل ملف CSS محلي
 ];
 
